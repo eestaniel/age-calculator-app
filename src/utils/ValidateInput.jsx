b094307cc5866edb867ac70430da58d5911a6f7e
EEstaniel <edwineestaniel93@gmail.com>
2023-05-26 19:04:10 -0700

    fix: re-did validate month for days with 28,29, 30 or 31 days

--- src/utils/ValidateInput.jsx
+++ src/utils/ValidateInput.jsx
@@ -1,15 +1,30 @@
 const ValidateInput = (date) => {
-    const { day, month, year } = date;
-    let isError = false;
-    let errors = { day: '', month: '', year: '' };
+    const {day, month, year} = date;
 
-    // Validate day
+    let errors = {day: '', month: '', year: ''};
+    let isError = false;
+
+    //take into account in month has 28 days, 30 days, or 31 days
+    // if library, check if valid leap year for 29 days
+
     if (!day) {
         isError = true;
         errors.day = 'This field is required';
-    } else if (day < 1 || day > 31) {
+    } else if ( day < 1 || day > 31) {
         isError = true;
         errors.day = 'Must be a valid day';
+    } else if ([4, 6, 9, 11].includes(Number(month)) && day > 30) {
+        isError = true;
+        errors.day = 'Must be a valid day';
+    } else if (month === 2) {
+        // check if valid leap year for 29 days
+        if (((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) && day > 29) {
+            isError = true;
+            errors.day = 'Must be a valid day';
+        } else if (day > 28) {
+            isError = true;
+            errors.day = 'Must be a valid day';
+        }
     }
 
     // Validate month
@@ -33,7 +48,7 @@ const ValidateInput = (date) => {
         errors.year = 'Must be in the future';
     }
 
-    return { isError, errors };
+    return {isError, errors};
 };
 
 export default ValidateInput;
